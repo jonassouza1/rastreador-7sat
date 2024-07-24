19f5852ee8c145d588dd9bc09fa58bd5e8724dff
Jonas Souza <Jonassouza305@yahoo.com.br>
2024-07-24 15:28:57 -0300

    --amend

--- pages/components/hero/index.tsx
+++ pages/components/hero/index.tsx
@@ -4,14 +4,15 @@ const Hero = () => {
     <section className={styles.section}>
       <div className={styles.divText}>
         <p>
-          A 7sat é uma empresa gaúcha que oferece os melhores serviços em
-          rastreamento veicular. Fundada em 13 de dezembro de 2017, inicialmente
-          com o nome de Rastrek Novo Hamburgo, a empresa vem crescendo cada vez
-          mais graças ao trabalho sério e de qualidade oferecido a seus
-          clientes. Com sede em Novo Hamburgo, no bairro Canudos, a empresa
-          presta serviços em todo o Vale dos Sinos, Vale do Paranhana, Vale do
-          Caí, Serra gaúcha, Porto Alegre e região metropolitana oferecendo o
-          melhor pra quem procura uma solução em rastreamento de veículos.
+          A 7sat Rastreamento é uma empresa gaúcha que oferece os melhores
+          serviços em rastreamento veicular. Fundada em 13 de dezembro de 2017,
+          inicialmente com o nome de Rastrek Novo Hamburgo, a empresa vem
+          crescendo cada vez mais graças ao trabalho sério e de qualidade
+          oferecido a seus clientes. Com sede em Novo Hamburgo, no bairro
+          Canudos, a empresa presta serviços em todo o Vale dos Sinos, Vale do
+          Paranhana, Vale do Caí, Serra gaúcha, Porto Alegre e região
+          metropolitana oferecendo o melhor pra quem procura uma solução em
+          rastreamento de veículos.
         </p>
       </div>
     </section>
